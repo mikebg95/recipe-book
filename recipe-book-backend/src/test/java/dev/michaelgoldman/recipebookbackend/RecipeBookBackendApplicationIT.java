d5fdf6fd6e22d7967608f6af12f276f632eced40
Michael <mikebgoldman95@gmail.com>
2026-07-03 18:01:32 +0200

    Added steps_number column to steps table (for ordering steps) with unique constraint. Cleaned up Testcontainer infrastructure setup; created AbstractIntegrationTest for integration tests to extend.

--- recipe-book-backend/src/test/java/dev/michaelgoldman/recipebookbackend/RecipeBookBackendApplicationIT.java
+++ recipe-book-backend/src/test/java/dev/michaelgoldman/recipebookbackend/RecipeBookBackendApplicationIT.java
@@ -1,12 +1,8 @@
 package dev.michaelgoldman.recipebookbackend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
-class RecipeBookBackendApplicationIT {
+class RecipeBookBackendApplicationIT extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
