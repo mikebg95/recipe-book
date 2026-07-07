cacbdc8cb47a53e11f595361848f8e47cdfd7d45
Michael <mikebgoldman95@gmail.com>
2026-07-07 10:47:36 +0200

    Setup: added webconfig class -> configured path with /api/v1 and (temporarily) disabled CORS

---
+++ backend/src/main/java/dev/michaelgoldman/recipebookbackend/config/WebConfig.java
@@ -0,0 +1,27 @@
+package dev.michaelgoldman.recipebookbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api/v1",
+                HandlerTypePredicate.forAnnotation(RestController.class)
+                        .and(HandlerTypePredicate.forBasePackage("org.springdoc").negate()));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
