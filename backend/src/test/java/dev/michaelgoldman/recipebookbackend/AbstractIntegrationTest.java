fcb28fc4edb7997bda37b6c40f77b22cca028600
Michael <mikebgoldman95@gmail.com>
2026-07-15 14:18:15 +0200

    - Created RecipeService.deleteById method via TDD. One happy path (delete successfully when recipe exists) and one non-happy path (throw RecipeDoesNotExistException when recipe does not exist). - Added TODO's concerning @Transactional annotations for Service methods. - Renamed RecipeRepositoryTest.java to RecipeRepositoryIT.java so it gets run during maven verify and not during maven test (it's fires up a TestContainer so should be considered an integration test). - Added cors.allowed-origin-pattern to application-test so integration test succeeds. Set the one in prod environment to Environment Variable.

--- backend/src/test/java/dev/michaelgoldman/recipebookbackend/AbstractIntegrationTest.java
+++ backend/src/test/java/dev/michaelgoldman/recipebookbackend/AbstractIntegrationTest.java
@@ -1,11 +1,12 @@
 package dev.michaelgoldman.recipebookbackend;
 
+import dev.michaelgoldman.recipebookbackend.config.WebConfig;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@Import(TestcontainersConfiguration.class)
+@Import({TestcontainersConfiguration.class, WebConfig.class})
 public abstract class AbstractIntegrationTest {
 }
